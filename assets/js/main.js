5484776248de19690644d5d8e8c80548241bac6c
Galiv04 <51029869+Galiv04@users.noreply.github.com>
2022-12-19 19:05:18 +0100

    bug fix dates

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ function getGDriveFiles(folderID) {
 
         if (lastModDate == "Invalid Date") {
           // undefined (because only today time)
-          lastModDate = new Date(nowDate.toLocaleDateString() + " " + lastModStr.substring(0, lastModStr.length-3));
+          lastModDate = nowDate;
         } else if (lastModDate.getFullYear() != thisYear) {
           // year info is missing
           lastModDate.setFullYear(thisYear);
